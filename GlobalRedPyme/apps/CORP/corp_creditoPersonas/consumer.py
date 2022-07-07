9b15063a78932fb65e0969c7c8bdea8495712648
Abraham Vallejos <abraham-rat@outlook.es>
2022-07-07 16:37:22 -0500

    Saved [Abraham Vallejos]:Saved methode produce and consumer changes.

--- GlobalRedPyme/apps/CORP/corp_creditoPersonas/consumer.py
+++ GlobalRedPyme/apps/CORP/corp_creditoPersonas/consumer.py
@@ -1,4 +1,3 @@
-
 import boto3
 import json
 # Importar configuraciones
@@ -7,37 +6,33 @@ from .serializers import CreditoPersonasSerializer
 from .models import CreditoPersonas
 from bson import ObjectId
 
+
 def get_queue_url():
     print('llega')
     region_name = config.AWS_REGION_NAME
     queue_name = config.AWS_QUEUE_NAME
     max_queue_messages = 10
     message_bodies = []
-    aws_access_key_id=config.AWS_ACCESS_KEY_ID
-    aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY
-    print('queue_name',queue_name)
+    aws_access_key_id = config.AWS_ACCESS_KEY_ID
+    aws_secret_access_key = config.AWS_SECRET_ACCESS_KEY
+    print('queue_name', queue_name)
     sqs = boto3.resource('sqs', region_name=region_name,
-            aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key)
+                         aws_access_key_id=aws_access_key_id,
+                         aws_secret_access_key=aws_secret_access_key)
     queue = sqs.get_queue_by_name(QueueName=queue_name)
     while True:
         messages_to_delete = []
         for message in queue.receive_messages(MaxNumberOfMessages=max_queue_messages):
             # process message body
             body = json.loads(message.body)
-            jsonRequest=json.loads(body['Message'])
-            print('josn',jsonRequest)
-            _idCredidPerson=json.loads(body['Message'])['_id']
-            print('josn2',json.loads(body['Message'])['_id'])
-            # print(json.loads(message.body))
+            jsonRequest = json.loads(body['Message'])
+            _idCredidPerson = json.loads(body['Message'])['_id']
             message_bodies.append(body)
             query = CreditoPersonas.objects.filter(pk=ObjectId(_idCredidPerson), state=1).first()
             serializer = CreditoPersonasSerializer(query, data=jsonRequest, partial=True)
 
             if serializer.is_valid():
-                print('va guardar')
                 serializer.save()
-                print('guarda')
             # add message to delete
             messages_to_delete.append({
                 'Id': message.message_id,
@@ -52,6 +47,4 @@ def get_queue_url():
         # handle any errors
         else:
             delete_response = queue.delete_messages(
-                    Entries=messages_to_delete)
-
-
+                Entries=messages_to_delete)
