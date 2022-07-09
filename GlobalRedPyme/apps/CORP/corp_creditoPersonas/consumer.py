6c690dda820f14ecc3964227d48c0e2dbcb93d36
Jimmy Yar <jimmy1817@hotmail.com>
2022-07-09 09:29:55 -0500

    Crear logs de cron sqs

--- GlobalRedPyme/apps/CORP/corp_creditoPersonas/consumer.py
+++ GlobalRedPyme/apps/CORP/corp_creditoPersonas/consumer.py
@@ -5,23 +5,30 @@ from apps.config import config
 from .serializers import CreditoPersonasSerializer
 from .models import CreditoPersonas
 from bson import ObjectId
-
+#logs
+from apps.CENTRAL.central_logs.methods import createLog,datosTipoLog, datosProductosMDP
+#declaracion variables log
+datosAux=datosProductosMDP()
+datosTipoLogAux=datosTipoLog()
+#asignacion datos modulo
+logModulo=datosAux['modulo']
+logApi=datosAux['api']
+#asignacion tipo de datos
+logTransaccion=datosTipoLogAux['transaccion']
+logExcepcion=datosTipoLogAux['excepcion']
 
 def get_queue_url():
-    print('llega')
-    region_name = config.AWS_REGION_NAME
-    queue_name = config.AWS_QUEUE_NAME
-    max_queue_messages = 10
-    message_bodies = []
-    aws_access_key_id = config.AWS_ACCESS_KEY_ID
-    aws_secret_access_key = config.AWS_SECRET_ACCESS_KEY
-    print('queue_name', queue_name)
-    sqs = boto3.resource('sqs', region_name=region_name,
-                         aws_access_key_id=aws_access_key_id,
-                         aws_secret_access_key=aws_secret_access_key)
-    queue = sqs.get_queue_by_name(QueueName=queue_name)
-    while True:
-        messages_to_delete = []
+    try:
+        region_name = config.AWS_REGION_NAME
+        queue_name = config.AWS_QUEUE_NAME
+        max_queue_messages = 10
+        aws_access_key_id = config.AWS_ACCESS_KEY_ID
+        aws_secret_access_key = config.AWS_SECRET_ACCESS_KEY
+        sqs = boto3.resource('sqs', region_name=region_name,
+                            aws_access_key_id=aws_access_key_id,
+                            aws_secret_access_key=aws_secret_access_key)
+        queue = sqs.get_queue_by_name(QueueName=queue_name)
+
         for message in queue.receive_messages(MaxNumberOfMessages=max_queue_messages):
             # process message body
             body = json.loads(message.body)
@@ -37,19 +44,14 @@ def get_queue_url():
                 serializer = CreditoPersonasSerializer(query, data=jsonRequest, partial=True)
             
             if serializer.is_valid():
+                # Guardamos
                 serializer.save()
-            # add message to delete
-            messages_to_delete.append({
-                'Id': message.message_id,
-                'ReceiptHandle': message.receipt_handle
-            })
-        
-        # if you don't receive any notifications the
-        # messages_to_delete list will be empty
-        if len(messages_to_delete) == 0:
-            break
-        # delete messages to remove them from SQS queue
-        # handle any errors
-        else:
-            delete_response = queue.delete_messages(
-                Entries=messages_to_delete)
+                # Borramos SQS
+                message.delete()
+        return 'completed'
+    except Exception as e: 
+        err={"error":'Un error ha ocurrido: {}'.format(e)}  
+        createLog(logModel,err,logExcepcion)
+        return err
+
+
