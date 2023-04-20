c0869ba92628f2046c61f8f5428103fea663b87e
Ariestefy <arieatefy@gmail.com>
2023-04-20 10:56:08 -0500

    [Arianna]Changed Url

--- GlobalRedPyme/apps/CORE/core_monedas/views.py
+++ GlobalRedPyme/apps/CORE/core_monedas/views.py
@@ -436,7 +436,7 @@ def insertarDato_monedas(dato):
                         USTED ACABA DE RECIBIR
                         {dato[4]} para que realice compras en los establecimientos afiliados pagando menos dinero en efectivo.
                         
-                        Regístrese a través del portal https://portal.bigpuntos.com y reciba sus Big Puntos de recompensa en su cuenta
+                        Regístrese a través del portal https://credicompra.bigpuntos.com y reciba sus Big Puntos de recompensa en su cuenta
                         
                         Atentamente,
                         CrediCompra - Big Puntos
@@ -452,7 +452,7 @@ def insertarDato_monedas(dato):
                         {dato[4]} para que realice compras en los establecimientos afiliados pagando menos dinero en efectivo.
                         </p>
                         <br>
-                        <p>Regístrese a través del portal https://portal.bigpuntos.com y reciba sus Big Puntos de recompensa en su cuenta</p>
+                        <p>Regístrese a través del portal https://credicompra.bigpuntos.com y reciba sus Big Puntos de recompensa en su cuenta</p>
                         <br>
                         Atentamente,
                         <br>
@@ -562,9 +562,9 @@ def insertarDato_monedasClientes(dato):
                         Por ser cliente de {empresa.nombreComercial} le regalamos {dato[4]} para que realice compras de 
                         productos de varias categorías en establecimientos afiliados PAGANDO MENOS DINERO EN EFECTIVO.
 
-                        Para acceder a sus Big Puntos, ingrese a: https://portal.bigpuntos.com/#/grp/login 
+                        Para acceder a sus Big Puntos, ingrese a: https://credicompra.bigpuntos.com/#/grp/login
 
-                        Si aún no tiene cuenta en Big Puntos, regístrese a través de: https://portal.bigpuntos.com/#/grp/registro y acceda a fabulosos premios.
+                        Si aún no tiene cuenta en Big Puntos, regístrese a través de: https://credicompra.bigpuntos.com/#/grp/registro y acceda a fabulosos premios.
 
                         Atentamente,
                         CrediCompra - Big Puntos
@@ -579,9 +579,9 @@ def insertarDato_monedasClientes(dato):
                         productos de varias categorías en establecimientos afiliados <b>PAGANDO MENOS DINERO EN EFECTIVO.</b>
                         </p>
                         <br>
-                        <p>Para acceder a sus Big Puntos, ingrese a: https://portal.bigpuntos.com/#/grp/login</p>
+                        <p>Para acceder a sus Big Puntos, ingrese a: https://credicompra.bigpuntos.com/#/grp/login</p>
                         <br>
-                        <p>Si aún no tiene cuenta en Big Puntos, regístrese a través de: https://portal.bigpuntos.com/#/grp/registro y acceda a fabulosos premios.</p>
+                        <p>Si aún no tiene cuenta en Big Puntos, regístrese a través de: https://credicompra.bigpuntos.com/#/grp/registro y acceda a fabulosos premios.</p>
                         <br>
                         Atentamente,
                         <br>
